c8ab9a12926132b4f0be798f404e08a3ba044f25
Michael Pope <michael@dtcorp.com.au>
2017-08-09 13:42:13 +1000

    Test-Fix up flaky stats spec

--- spec/flaky_stats_spec.rb
+++ spec/flaky_stats_spec.rb
@@ -1,9 +1,11 @@
 require "spec_helper"
+require 'date'
 
 RSpec.describe FlakyStats do
   describe "#form_data" do
-    before do 
-      Timecop.freeze(Time.now)
+    before do
+      @time=Time.now
+      Timecop.freeze(@time)
       @flaky_tests = FlakyStats::FlakyTests.new
       @failed_file = {filename: "foo_spec.rb", lineno: "12"}
     end
@@ -13,15 +15,18 @@ RSpec.describe FlakyStats do
     end
     
     it "includes datetime" do
-      expect(@flaky_tests.form_data(@failed_file)[0]).to eq(Time.now)
+      format = "%d/%m/%Y %H:%M"
+      result = @flaky_tests.form_data(@failed_file).split(",")[0]
+      result = Time.parse(result).strftime(format)
+      expect(result).to eq(@time.strftime(format))
     end
 
     it "includes filename" do 
-      expect(@flaky_tests.form_data(@failed_file)[1]).to eq("foo_spec.rb")
+      expect(@flaky_tests.form_data(@failed_file).split(",")[1]).to eq("foo_spec.rb")
     end
 
     it "includes lineno" do 
-      expect(@flaky_tests.form_data(@failed_file)[2]).to eq("12")
+      expect(@flaky_tests.form_data(@failed_file).split(",")[2]).to eq("12")
     end
   end
 end
